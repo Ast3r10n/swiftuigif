ca34ec81b9f0a3c12a870736f68f3781ab88932a
Andrea Sacerdoti <andrea.sacerdoti@mobilesoft.it>
2020-09-16 00:36:30 +0200

    Add ImageSourceProvider

--- Sources/SwiftUIGif/GifView.swift
+++ Sources/SwiftUIGif/GifView.swift
@@ -8,34 +8,88 @@
 import SwiftUI
 
 struct GifView: View {
-  var name: String
-  var numberOfFrames: Int
-  var delay: Double = 0.2
-  @State private var currentFrame = 0
+  class ImageSourceProvider {
+    var source: CGImageSource?
+    var numberOfFrames: Int?
+    var name: String
+    var delay: Double
 
-  fileprivate var updateFunction: ((inout Int, Int) -> Void)?
+    init(name: String, delay: Double) {
+      self.name = name
+      self.delay = delay
+      self.source = imageSource(named: name)
+      self.numberOfFrames = frameCount()
+    }
+
+    private func imageSource(named imageName: String) -> CGImageSource? {
+      guard let bundleURL = Bundle.main.url(forResource: imageName, withExtension: "gif"),
+            let imageData = try? Data(contentsOf: bundleURL) else {
+        print("Cannot turn image named \"\(imageName)\" into NSData")
+        return nil
+      }
+
+      let gifOptions = [
+        kCGImageSourceShouldAllowFloat as String : true as NSNumber,
+        kCGImageSourceCreateThumbnailWithTransform as String : true as NSNumber,
+        kCGImageSourceCreateThumbnailFromImageAlways as String : true as NSNumber
+      ] as CFDictionary
+
+      guard let imageSource = CGImageSourceCreateWithData(imageData as CFData, gifOptions) else {
+        debugPrint("Cannot create image source with data!")
+        return nil
+      }
+
+      return imageSource
+    }
+
+    func frameCount() -> Int {
+      if let source = source {
+        return CGImageSourceGetCount(source)
+      }
+
+      return 0
+    }
+
+    func frame(at index: Int) -> Image? {
+      guard let imageSource = source,
+            let cgImageRef = CGImageSourceCreateImageAtIndex(imageSource, index, nil) else {
+        return nil
+      }
+      return Image(cgImageRef, scale: 1.0, label: Text(""))
+    }
+  }
+
+  @State var currentFrame = 0
+  @State var direction = false
+  var provider: ImageSourceProvider
 
   var body: some View {
-    Image("\(name)\(currentFrame)")
+    //Image("\(name)\(currentFrame)")
+    provider.frame(at: currentFrame)?
       .resizable()
-      .onReceive(Timer.publish(every: delay,
-                               on: .main,
-                               in: .common)
-                  .autoconnect()) { _ in
-        if let updateFunction = updateFunction {
-          updateFunction(&currentFrame, numberOfFrames)
+      .onReceive(Timer.publish(every: provider.delay, on: .main, in: .common).autoconnect()) { _ in
+        if self.direction {
+          self.currentFrame = (self.currentFrame == 0) ? self.provider.numberOfFrames! - 1 : self.currentFrame - 1
         } else {
-          currentFrame = (currentFrame + 1) % numberOfFrames
+          self.currentFrame = (self.currentFrame + 1 ) % self.provider.numberOfFrames!
         }
       }
   }
 }
 
-extension GifView {
-  func reversed() -> GifView {
-    return GifView(name: self.name,
-                   numberOfFrames: self.numberOfFrames,
-                   delay: self.delay,
-                   updateFunction: { $0 = $0 == 0 ? $1 : ($0 - 1) })
-  }
-}
+//struct GifViewModifier: ViewModifier {
+//  typealias Body = GifView
+//
+//  func body(content: Content) -> GifView {
+//
+//  }
+//}
+//
+//extension GifView {
+//  func reversed() -> GifView {
+//    return GifView(name: self.name,
+//                   numberOfFrames: self.numberOfFrames,
+//                   delay: self.delay,
+//                   updateFunction: { $0 = $0 == 0 ? $1 : ($0 - 1) })
+//  }
+//}
